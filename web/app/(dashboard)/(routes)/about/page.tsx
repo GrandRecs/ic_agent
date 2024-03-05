7c6c786bbf824ec5264cf2173705e35510d2e7a7
zezhuyu <zezhuyu@icloud.com>
2024-03-04 19:25:31 -0500

    update about page

--- web/app/(dashboard)/(routes)/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-        <div className="text-center p-8 h-screen">
-            <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-                About Us
-            </h2>
-        </div>
-    );
-}
--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
-                            <a href="/about" className="hover:underline me-4 md:me-6">About</a>
+                            <a href="https://github.com/GrandRecs/ic_agent/blob/main/README.md" className="hover:underline me-4 md:me-6">About</a>
                         </li>
                         <li>
                             <a href="https://github.com/GrandRecs/ic_agent?tab=GPL-3.0-1-ov-file" className="hover:underline me-4 md:me-6">Licensing</a>
